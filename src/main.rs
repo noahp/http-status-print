3b6fcf8203096c7ab4ebe911746d99d5adace684
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2018-03-17 16:09:45 -0400

    Add base files

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate clap;
+extern crate http;
+
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Noah P https://github.com/noahp")
+        .about("Print information about HTTP status codes")
+        .arg(
+            Arg::with_name("CODE")
+                .takes_value(true)
+                .multiple(true)
+                .required(true)
+                .help("Input codes to return information on"),
+        )
+        .get_matches();
+
+    let codes: Vec<_> = matches.values_of("CODE").unwrap().collect();
+
+    for code in codes {
+        let status = http::status::StatusCode::from_u16(code.parse().unwrap()).unwrap();
+
+        println!("{}", status);
+    }
+}
