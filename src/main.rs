76338644ebad4987d2aeb70401296f27f7df64fd
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2020-04-13 15:06:15 -0400

    Switch to structopt
    Nice and concise for basic stuff like this

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-use clap::{App, Arg};
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
+
+#[structopt(global_settings = &[AppSettings::ColoredHelp])]
+#[derive(Debug, StructOpt)]
+struct Opt {
+    code: Vec<u16>,
+}
 
 fn main() {
     human_panic::setup_panic!();
 
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("Noah Pendleton / https://github.com/noahp")
-        .about("Print information about HTTP status codes")
-        .arg(
-            Arg::with_name("CODE")
-                .takes_value(true)
-                .multiple(true)
-                .required(true)
-                .help("Numerical HTTP codes to return information on"),
-        ).get_matches();
-
-    let codes: Vec<&str> = matches.values_of("CODE").unwrap().collect();
-
-    for code in codes {
-        // Handle u16 parsing errors with a friendly response
-        let intcode: u16 = match code.parse() {
+    let opt = Opt::from_args();
+    for code in opt.code {
+        // Similar for codes that aren't present in the http::status list
+        let status = match http::status::StatusCode::from_u16(code) {
             Ok(n) => n,
             Err(t) => {
                 println!("bad error code \"{}\": {}", code, t);
@@ -27,22 +21,14 @@ fn main() {
             }
         };
 
-        // Similar for codes that aren't present in the http::status list
-        let status = match http::status::StatusCode::from_u16(intcode) {
-            Ok(n) => n,
-            Err(t) => {
-                println!("bad error code \"{}\": {}", intcode, t);
-                continue;
-            }
-        };
-
         // Only print a link to the expanded descriptions for known codes
         match status.canonical_reason() {
+            // TODO gimme some nice colorization here please!
             Some(reason) => println!(
                 "{} - {} : https://httpstatuses.com/{}",
-                intcode, reason, intcode
+                code, reason, code
             ),
-            None => println!("{} - {}", intcode, status),
+            None => println!("{} - {}", code, status),
         };
     }
 }
