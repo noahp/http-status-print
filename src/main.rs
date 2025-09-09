b567ed0a443fbe65e1cd8999bd4c2da9b8d70cdf
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2025-09-09 11:41:20 -0400

    Add ci

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::{Parser};
+use clap::Parser;
+use colored::*;
 
 #[derive(Debug, Parser)]
 #[command(color = clap::ColorChoice::Always)]
@@ -22,11 +23,20 @@ fn main() {
 
         // Only print a link to the expanded descriptions for known codes
         match status.canonical_reason() {
-            // TODO gimme some nice colorization here please!
-            Some(reason) => println!(
-                "{} - {} : https://httpstatuses.com/{}",
-                code, reason, code
-            ),
+            Some(reason) => {
+                let colored_code = match code {
+                    100..=199 => code.to_string().blue(),    // Informational
+                    200..=299 => code.to_string().green(),   // Success
+                    300..=399 => code.to_string().yellow(),  // Redirection
+                    400..=499 => code.to_string().red(),     // Client Error
+                    500..=599 => code.to_string().magenta(), // Server Error
+                    _ => code.to_string().white(),           // Unknown
+                };
+                println!(
+                    "{} - {} : https://httpstatuses.com/{}",
+                    colored_code, reason, code
+                )
+            }
             None => println!("{} - {}", code, status),
         };
     }
